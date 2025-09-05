7806da78ef23209f62f324d7f7db3fb24ea23dca
dhyanpatel3 <dp28dhyan@gmail.com>
2025-09-05 12:50:37 +0530

    add server.js

--- server/server.js
+++ server/server.js
@@ -51,7 +51,8 @@ app.use("/api/messages", messageRouter);
 //connect to database
 await connectDB();
 
-if(process.env.NODE_ENV==="production"){
+
+if(process.env.NODE_ENV !=="production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
